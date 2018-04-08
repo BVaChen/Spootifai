882157454d1fd80c8b7a243553f400180e5db317
Ben Chen <36746639+Luunix@users.noreply.github.com>
2018-04-08 18:15:46 -0400

    New Repo

---
+++ CNIT155.Group39-Project/CNIT155.Group39-Project/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace CNIT155.Group39_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnBackward = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.txtStuff = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radShuffle = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.CausesValidation = false;
+            this.label1.Font = new System.Drawing.Font("Magneto", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(310, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(485, 75);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MUSIC PLAYER 39";
+            // 
+            // btnImport
+            // 
+            this.btnImport.AutoEllipsis = true;
+            this.btnImport.BackColor = System.Drawing.Color.DarkViolet;
+            this.btnImport.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnImport.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnImport.Location = new System.Drawing.Point(318, 135);
+            this.btnImport.Margin = new System.Windows.Forms.Padding(6);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(236, 75);
+            this.btnImport.TabIndex = 1;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = false;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.DarkMagenta;
+            this.btnPlay.Location = new System.Drawing.Point(626, 417);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(6);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(150, 44);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            // 
+            // btnForward
+            // 
+            this.btnForward.BackColor = System.Drawing.Color.Violet;
+            this.btnForward.Location = new System.Drawing.Point(788, 417);
+            this.btnForward.Margin = new System.Windows.Forms.Padding(6);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(150, 44);
+            this.btnForward.TabIndex = 2;
+            this.btnForward.Text = "> >";
+            this.btnForward.UseVisualStyleBackColor = false;
+            // 
+            // btnBackward
+            // 
+            this.btnBackward.BackColor = System.Drawing.Color.Violet;
+            this.btnBackward.Location = new System.Drawing.Point(464, 417);
+            this.btnBackward.Margin = new System.Windows.Forms.Padding(6);
+            this.btnBackward.Name = "btnBackward";
+            this.btnBackward.Size = new System.Drawing.Size(150, 44);
+            this.btnBackward.TabIndex = 2;
+            this.btnBackward.Text = "< <";
+            this.btnBackward.UseVisualStyleBackColor = false;
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.Color.Violet;
+            this.btnPause.Location = new System.Drawing.Point(626, 473);
+            this.btnPause.Margin = new System.Windows.Forms.Padding(6);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(150, 44);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = false;
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.ItemHeight = 25;
+            this.lstOutput.Location = new System.Drawing.Point(35, 135);
+            this.lstOutput.Margin = new System.Windows.Forms.Padding(6);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(271, 579);
+            this.lstOutput.TabIndex = 3;
+            // 
+            // txtStuff
+            // 
+            this.txtStuff.Location = new System.Drawing.Point(322, 222);
+            this.txtStuff.Margin = new System.Windows.Forms.Padding(6);
+            this.txtStuff.Name = "txtStuff";
+            this.txtStuff.Size = new System.Drawing.Size(232, 31);
+            this.txtStuff.TabIndex = 4;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(318, 671);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(6);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(84, 44);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(318, 603);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(6);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(84, 44);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(318, 435);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(6);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(84, 44);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(318, 367);
+            this.btnSort.Margin = new System.Windows.Forms.Padding(6);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(84, 44);
+            this.btnSort.TabIndex = 2;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Violet;
+            this.btnStop.Location = new System.Drawing.Point(626, 361);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(6);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(150, 44);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(566, 209);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(6);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(117, 44);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1229, -424);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 31);
+            this.textBox1.TabIndex = 4;
+            // 
+            // radShuffle
+            // 
+            this.radShuffle.AutoSize = true;
+            this.radShuffle.Location = new System.Drawing.Point(322, 287);
+            this.radShuffle.Name = "radShuffle";
+            this.radShuffle.Size = new System.Drawing.Size(110, 29);
+            this.radShuffle.TabIndex = 5;
+            this.radShuffle.TabStop = true;
+            this.radShuffle.Text = "Shuffle";
+            this.radShuffle.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1071, 763);
+            this.Controls.Add(this.radShuffle);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtStuff);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.btnBackward);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Spootifai";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnBackward;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.TextBox txtStuff;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radShuffle;
+    }
+}
+
---
+++ CNIT155.Group39-Project/CNIT155.Group39-Project/Form1.cs
@@ -0,0 +1,48 @@
+﻿/* --------------------------------------------------------------------------------------------
+ * Group 39
+ * Ben Chen, Grace Chen, Iris Chu, Jason Lam
+ * 
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace CNIT155.Group39_Project
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer Music = new WindowsMediaPlayer();
+        private const int mSize = 30;
+        private string[] mSongs = new string[mSize];
+        private int mIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Open = new OpenFileDialog();
+
+            if (Open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string fileName = Open.FileName;
+
+                txtStuff.Text = fileName;
+                Music.URL = fileName;
+            }
+        }
+
+
+    }
+    
+}
